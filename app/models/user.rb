1f81b0caf229940b61710c07830583d4e2e585a8
evsandrey <evs.andrey@gmail.com>
2015-07-27 20:10:26 +0300

    redesign

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,9 +38,9 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar,
   	styles: { menu: "20x20#", thumb: "50x50#", med: "300x300#"},
-  	:path => "trip-site/user/:id/:style.:extension",
-    :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  	:storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :dropbox_options => {:path => proc { |style| "users/#{id}/#{file.original_filename}" } }
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
