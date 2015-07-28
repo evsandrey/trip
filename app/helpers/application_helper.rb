fc17b7cdeb55e0e87694901eabd5d6f1140ad640
evsandrey <evs.andrey@gmail.com>
2015-07-28 16:42:25 +0300

    mfg_and_weight_added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,7 @@ module ApplicationHelper
 	def small_trophy_badge(trophy)
 		render partial: "shared/smalltrophy", locals: { trophy: trophy }
 	end
+	def small_bait_badge(bait)
+		render partial: "shared/smallbait", locals: { bait: bait }
+	end
 end
