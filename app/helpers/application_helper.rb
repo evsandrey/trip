ef501e15177004d7ebe53ff53213c7c8b758e84d
evsandrey <evs.andrey@gmail.com>
2015-07-30 19:07:20 +0300

    mfg_and_weight_added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
 			    
 			    pic.gsub(/[\(\)]/,"").split(",").each do |pict|
 			    	if photo = Picture.where(id: pict).first
-			    		gallery_code += '<div class="item"><img src="' + photo.photo(:grid) + '"/></div>'
+			    		gallery_code += '<div class="item"><img src="' + photo.photo(:slide) + '"/></div>'
 			    	end
 			    end
 			    
