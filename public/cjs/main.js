8bb1e686f23969dba3424e9aa440d74ad1aa27ac
evsandrey <evs.andrey@gmail.com>
2015-07-31 15:27:09 +0300

    mfg_and_weight_added

--- public/cjs/main.js
+++ public/cjs/main.js
@@ -6,10 +6,15 @@ jQuery(document).ready(function($){
 	hideBlocks(timelineBlocks, offset);
 
 	//on scolling, show/animate timeline blocks when enter the viewport
-	$('#timeline').on('scroll', function(){
-		(!window.requestAnimationFrame) 
-			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
-			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	$(window).on('scroll', function(){
+			if( $(this).offset().top < $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').is('.is-hidden, .bounce-out') ) {
+                    $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden bounce-out').addClass('bounce-in');
+            }
+			if($(this).offset().top >= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').is('.bounce-in')) {
+				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('bounce-in').addClass('bounce-out');   
+			} else if($(this).offset().top >= $(window).scrollTop()+$(window).height()*0.80 && $(this).find('.cd-timeline-img').is('.bounce-out')) {
+				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('bounce-out').addClass('is-hidden'); 
+			}
 	});
 
 	function hideBlocks(blocks, offset) {
