787b4e6b980777a52182c68be6057f2c229c84e6
evsandrey <evs.andrey@gmail.com>
2015-07-31 15:22:14 +0300

    mfg_and_weight_added

--- public/cjs/main.js
+++ public/cjs/main.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($){
 	hideBlocks(timelineBlocks, offset);
 
 	//on scolling, show/animate timeline blocks when enter the viewport
-	$(window).on('scroll', function(){
+	$('#timeline').on('scroll', function(){
 		(!window.requestAnimationFrame) 
 			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
 			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
