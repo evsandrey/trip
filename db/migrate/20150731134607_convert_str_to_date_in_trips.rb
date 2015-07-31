146ed0ddf18b5d2d19afba95ee8ca040b6ac60c3
evsandrey <evs.andrey@gmail.com>
2015-07-31 18:04:08 +0300

    mfg_and_weight_added

--- db/migrate/20150731134607_convert_str_to_date_in_trips.rb
+++ db/migrate/20150731134607_convert_str_to_date_in_trips.rb
@@ -1,6 +1,5 @@
 class ConvertStrToDateInTrips < ActiveRecord::Migration
   def change
   	change_column :trips, :sdate, 'date USING to_date(sdate::text, \'MM/DD/YYYY\')'
-  	change_column :trips, :sdate, 'date USING to_date(sdate::text, \'MM/DD/YYYY\')'
   end
 end
