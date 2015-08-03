f6fab463a3c72145d90ff0fb380b5157ef729c83
evsandrey <evs.andrey@gmail.com>
2015-08-03 17:16:32 +0300

    cancan_added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 #before_filter :authenticate_user!
   load_and_authorize_resource
   autocomplete :user, :nickname, :full => true, :extra_data => [:id]
-  
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   def show
     @user = User.find(params[:id])
 
@@ -11,8 +11,14 @@ class UsersController < ApplicationController
         format.xml { render :xml => @user }
     end
   end
+
+  def edit
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
-    
+  def set_user
+      @user = User.find(params[:id])
+  end  
   
 end
