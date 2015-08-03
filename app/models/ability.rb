eb42e027add9de6a248de89c3b0c49fb682b7496
evsandrey <evs.andrey@gmail.com>
2015-08-03 19:34:40 +0300

    cancan_rights

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -23,7 +23,7 @@ class Ability
          can :create, Place
          can :create, Trophy
          can :create, Trip
-         can :update, Trip, user.id => { user_id => :trip_users_ids }
+         can :update, Trip, :trip_users_ids => user.id
         end
 
 
