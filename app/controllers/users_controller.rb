39203c6be1c29d8b6f81dee115ebc2b586643dc7
evsandrey <evs.andrey@gmail.com>
2015-08-03 17:33:20 +0300

    cancan_added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,18 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'Trophy was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   
   private
     # Use callbacks to share common setup or constraints between actions.
