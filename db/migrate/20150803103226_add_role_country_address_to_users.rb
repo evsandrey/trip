180ce79fb3f77f6bac85ac018da10031d7e725b7
evsandrey <evs.andrey@gmail.com>
2015-08-03 13:39:24 +0300

    cancan_added

--- db/migrate/20150803103226_add_role_country_address_to_users.rb
+++ db/migrate/20150803103226_add_role_country_address_to_users.rb
@@ -1,7 +1,7 @@
 class AddRoleCountryAddressToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :role, :string
-    add_column :users, :country, :string
+    add_column :users, :role, :string, :default => "user"
+    add_column :users, :country, :string, :default => "RU"
     add_column :users, :address, :string
   end
 end
