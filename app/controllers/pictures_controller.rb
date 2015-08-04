023be8cd559611e830e1d12fd4b8e0ec2f5ba118
evsandrey <evs.andrey@gmail.com>
2015-08-04 15:29:28 +0300

    cancan_rights

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -71,7 +71,7 @@ class PicturesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def picture_params
-      params.require(:picture).permit(:user_id, :name, :trip_id)
+      
     end
   def to_fileupload(picture)
   {
