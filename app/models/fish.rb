3c5e0b0d524abb6d16da752e0003f6bc0ea4abea
evsandrey <evs.andrey@gmail.com>
2015-08-17 20:45:05 +0300

    css

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -22,4 +22,8 @@ class Fish < ActiveRecord::Base
     	:dropbox_visibility => 'public',
 	    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
 	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+
+	def photo_url
+	    photo.url(:medium)
+	end
 end
